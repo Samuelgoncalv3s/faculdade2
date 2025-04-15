33da9ceaeebd865a6fe0a2a536175b69ffce2798
Samuel <goncalvessozial09@gmail.com>
2025-04-14 21:00:50 -0300

    Alteracao Atv Soma

---
+++ AlgoritimoDeSoma.java
@@ -0,0 +1,10 @@
+public class Aula01 {
+    public static void main(String[] agrs){
+        float number1,number2,result;
+        number1 = 20;
+        number2 = 30;
+        result = number1 + number2;
+        System.out.print("Seu resultado é de : "+ result);
+
+    }
+}
